7685effc9d990e74387d61a691cab44a8a5e75c8
Feng <ff520@live.fr>
2017-06-15 22:59:57 +0200

    Delete start.py

--- start.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-import pip
-module_dependances = ["urllib3",'hello']
-for m in module_dependances:
-	if not (m in sys.modules):
-		print(m+' n\'est pas installé')
